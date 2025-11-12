1d3b84cb90badec459d02a5fbd14f27efef9cb82
Himasha Dewmini <himashadewmini12345@gmail.com>
2025-11-12 09:49:39 +0530

    Update studentdb.sql

--- studentManagement/studentdb.sql
+++ studentManagement/studentdb.sql
@@ -4,7 +4,7 @@ DROP DATABASE IF EXISTS studentdb;
 CREATE DATABASE studentdb;
 USE studentdb;
 
--- Table structure for table `students`
+
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -16,7 +16,7 @@ CREATE TABLE `students` (
   UNIQUE KEY `UKe2rndfrsx22acpq2ty1caeuyw` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Insert student data with updated names
+
 INSERT INTO `students` (`id`,`age`,`course`,`email`,`name`) VALUES
 (1, 24, 'BICT', 'himasha@gmail.com', 'Himasha Dewmini'),
 (2, 19, 'BICT', 'hasini@gmail.com', 'Hasini Sankalpana'),
